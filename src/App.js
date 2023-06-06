c8883430fa32628387a23ff82816089cb994b28f
Anish <kirodiwlanish11@gmail.com>
2023-06-06 20:20:10 +0530

    User profile updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,153 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faTrashAlt, faPencilAlt, faEnvelope, faPhone, faGlobe } from '@fortawesome/free-solid-svg-icons';
+import Wait from './Component/waiting';
+import EditPopup from './Component/EditPopup';
 
 function App() {
+  const [userProfiles, setUserProfiles] = useState([]);
+  const [editUserId, setEditUserId] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    let fetchTimeout;
+
+    const fetchUsers = async () => {
+      try {
+        // Fetching user data from API
+
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        const users = response.data.slice(0, 10);
+
+        // generating avatar using Dicebar API
+
+        const updatedUserProfiles = await Promise.all(
+          users.map(async (user) => {
+            const avatarResponse = await axios.get(`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`, {
+              params: {
+                background: 'transparent',
+                width: 200,
+                height: 200,
+              },
+            });
+
+            return {
+              id: user.id,
+              username: user.username,
+              email: user.email,
+              phone: user.phone,
+              website: user.website,
+              avatar: avatarResponse.config.url,
+              liked: false,
+            };
+          })
+        );
+
+        setUserProfiles(updatedUserProfiles);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchTimeout = setTimeout(() => {
+      fetchUsers();
+    }, 1500);
+
+    return () => clearTimeout(fetchTimeout);
+  }, []);
+
+  const handleLike = (userId) => {
+    setUserProfiles((prevUserProfiles) =>
+      prevUserProfiles.map((user) =>
+        user.id === userId ? { ...user, liked: !user.liked } : user
+      )
+    );
+  };
+
+  const handleDelete = (userId) => {
+    setUserProfiles((prevUserProfiles) =>
+      prevUserProfiles.filter((user) => user.id !== userId)
+    );
+  };
+
+  const handleEdit = (userId) => {
+    setEditUserId(userId);
+  };
+
+  const handleSaveEdit = (editedUser) => {
+    setUserProfiles((prevUserProfiles) =>
+      prevUserProfiles.map((user) =>
+        user.id === editUserId ? { ...user, ...editedUser } : user
+      )
+    );
+    setEditUserId(null);
+  };
+
+  const handleCancelEdit = () => {
+    setEditUserId(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {isLoading ? (
+        <div className="loadingSpinner">
+          <Wait />
+        </div>
+      ) : (
+        <div className="userList">
+          {userProfiles.map((user) => (
+            <div key={user.id} className="userCard">
+              <div className="avatarSection">
+                <img src={user.avatar} alt={`Avatar for ${user.username}`} className="avatar" />
+              </div>
+              <div className="userDetails">
+                <p className="username">{user.username}</p>
+                <p className="userDetail">
+                  <FontAwesomeIcon icon={faEnvelope} className="icon" />
+                  {user.email}
+                </p>
+                <p className="userDetail">
+                  <FontAwesomeIcon icon={faPhone} className="icon" />
+                  {user.phone}
+                </p>
+                <p className="userDetail">
+                  <FontAwesomeIcon icon={faGlobe} className="icon" />
+                  {user.website}
+                </p>
+              </div>
+              <div className="actions">
+                <span
+                  className={`actionIcon ${user.liked ? 'liked' : ''}`}
+                  onClick={() => handleLike(user.id)}
+                >
+                  <FontAwesomeIcon
+                    icon={faHeart}
+                    className={`heartIcon ${user.liked ? 'fill' : ''}`}
+                  />
+                </span>
+                <span className="actionIcon" onClick={() => handleEdit(user.id)}>
+                  <FontAwesomeIcon icon={faPencilAlt} className="editIcon" />
+                </span>
+                <span className="actionIcon" onClick={() => handleDelete(user.id)}>
+                  <FontAwesomeIcon icon={faTrashAlt} className="trashIcon" />
+                </span>
+
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {editUserId && (
+        <EditPopup
+          user={userProfiles.find((user) => user.id === editUserId)}
+          onSave={handleSaveEdit}
+          onCancel={handleCancelEdit}
+        />
+      )}
     </div>
   );
 }
---
+++ src/Component/EditPopup.js
@@ -0,0 +1,95 @@
+import React from "react";
+import "./EditPopup.css"
+import { useState } from "react";
+
+function EditPopup({ user, onSave, onCancel }) {
+    const [editedUser, setEditedUser] = useState({
+        username: user.username,
+        email: user.email,
+        phone: user.phone,
+        website: user.website,
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setEditedUser((prevEditedUser) => ({ ...prevEditedUser, [name]: value }));
+    };
+
+    const handleSave = (e) => {
+        e.preventDefault();
+        onSave(editedUser);
+    };
+
+    return (
+        <div className="editOverlay">
+            <div className="editPopup">
+                <h2>Edit User</h2>
+                <form onSubmit={handleSave}>
+                    <div className="formGroup">
+                        <label htmlFor="username">
+                            <span className="required">*</span> Username :
+                        </label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            value={editedUser.username}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div className="formGroup">
+                        <label htmlFor="email">
+                            <span className="required">*</span> Email :
+                        </label>
+                        <input
+                            type="text"
+                            id="email"
+                            name="email"
+                            value={editedUser.email}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div className="formGroup">
+                        <label htmlFor="phone">
+                            <span className="required">*</span> Phone :
+                        </label>
+                        <input
+                            type="text"
+                            id="phone"
+                            name="phone"
+                            value={editedUser.phone}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div className="formGroup">
+                        <label htmlFor="website">
+                            <span className="required">*</span> Website :
+                        </label>
+                        <input
+                            type="text"
+                            id="website"
+                            name="website"
+                            value={editedUser.website}
+                            onChange={handleInputChange}
+                            required
+                        />
+                    </div>
+                    <div className="buttons">
+                        <button className="cancelButton" onClick={onCancel}>
+                            Cancel
+                        </button>
+                        <button className="saveButton" type="submit">
+                            Ok
+                        </button>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default EditPopup;
